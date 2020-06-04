7e334999ddd5c1ae5c1951bcdd0f444b2646009b
Alan <alan@Alans-MBP.lan>
2020-06-04 23:30:27 +0800

    feat: 完善 tree 命令

--- src/bin/commands/hash.ts
+++ src/bin/commands/hash.ts
@@ -1,9 +1,10 @@
 import { recursiveFiles } from "../../library";
 import path from "path";
+import { Options } from "yargs";
 
 export const command = "hash [dir]";
 export const desc = "";
-export const builder = {
+export const builder: { [key: string]: Options } = {
   dir: {
     default: '.',
   }
@@ -20,3 +21,25 @@ export const handler = function (argv: any) {
   })).map(item => `${item.path}(${item.size})`);
   console.log(files.join("\n"));
 }
+
+
+
+/**
+import { hashFile, hashFiles } from "../library";
+
+const hub = hashFiles("/Users/alan/workspace/temporary/Music", {
+  recursive: {
+    filter: item => item.path.indexOf("AlbumArt") === -1,
+    recursive: true
+  },
+  hash: {
+    chunkSize: 1024 * 1024
+  }
+});
+hub.on("log", data => {
+  console.log(data.status, data.file.path);
+})
+hub.on("data", data => {
+  console.log(data.hash, data.file.path);
+});
+ */
--- src/bin/commands/tree.ts
+++ src/bin/commands/tree.ts
@@ -1,22 +1,33 @@
 import { recursiveFiles } from "../../library";
 import path from "path";
+import { Options } from "yargs";
 
 export const command = "tree [dir]";
 export const desc = "列出目录树";
-export const builder = {
+export const builder: { [key: string]: Options } = {
   dir: {
-    default: '.',
+    default: "./",
+    alias: "d",
+    describe: "目录"
+  },
+  recursive: {
+    default: true,
+    alias: "r",
+    describe: "是否递归"
+  },
+  ignore: {
+    default: "node_modules|\\.git"
   }
 };
 export const handler = function (argv: any) {
   const baseDir = path.resolve(argv.dir);
+  // console.log(`${argv.recursive ? "递归" : ""}列出${baseDir}目录下文件`);
 
-  const files = recursiveFiles(argv.dir, {
-    recursive: true,
-    filter: item => item.path.indexOf("node_modules") === -1
-  }).map(item => ({
-    path: item.path.substr(baseDir.length + 1),
-    size: item.stat.isFile() ? item.stat.size : "--"
-  })).map(item => `${item.path}(${item.size})`);
-  console.log(files.join("\n"));
+  recursiveFiles(argv.dir, {
+    recursive: argv.recursive,
+    filter: item => !new RegExp(argv.ignore).test(item.path)
+  }).forEach(item => {
+    const path = item.path.substr(baseDir.length);
+    console.log(`${path}(${item.stat.size})`);
+  });
 }
